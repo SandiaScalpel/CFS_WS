5546bfaeb9d24d9616daae1c9390e8c5cb7eba33
SandiaScalpel <sdcoston@gmail.com>
2019-01-27 20:52:41 -0700

    changed arduino sketch to roll through a cycle of increasingly fast flashes

--- sketch_nov25a/sketch_nov25a.ino
+++ sketch_nov25a/sketch_nov25a.ino
@@ -1,12 +1,25 @@
 #include <Arduino.h>
 
-    void setup() {
-        pinMode(13, OUTPUT);
-    }
+void setup() {
+    pinMode(13, OUTPUT);
+    blinks(2);
+}
 
-    void loop() {
-        digitalWrite(13, 0);
-        delay(1000);
-        digitalWrite(13, 1);
-        delay(4000);
+void loop() {
+    
+  
+  for (int i=1; i <= 25; i++){
+    for (int j=1; j <= i; j++){
+      blinks(i);
     }
+    delay(1500);
+  }
+}
+
+void blinks(int d)
+{
+   digitalWrite(13, 1);
+   delay(1000/d);
+   digitalWrite(13, 0);
+   delay(1000/d);
+}
